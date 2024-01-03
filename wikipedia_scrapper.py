573a6f14dc8dcfb0df2eadd13df7540676b9c089
M1 <96156886+M1-CHAEL@users.noreply.github.com>
2024-01-02 17:55:10 -0700

    Update wikipedia_scrapper.py

--- wikipedia_scrapper.py
+++ wikipedia_scrapper.py
@@ -5,13 +5,16 @@ def scrapeWikiArticle(url):
 	response = requests.get(url=url,)
 	soup = BeautifulSoup(response.content, 'html.parser')
 	
-	allLinks = soup.find(id="mw-content-text").find_all("a")
+	allLinks = soup.find(id="bodyContent").find_all("a")
+	linkToScrape = 0
 	
 	for link in allLinks:
 		if link['href'].find("/wiki/") == -1:
-			print(link)
-		else:
-			break
+			continue
+		linkToScrape = link
+		break
+	print("http://en.wikipedia.com" + link['href'])
 	
-
-scrapeWikiArticle("https://en.wikipedia.org/wiki/Template:US_Army_navbox")
+#use below scrapper function on not protected wikis, i.e. not military wikis as you will be blocked :(
+#for more information https://en.wikipedia.org/wiki/Wikipedia:Protection_policy
+#scrapeWikiArticle("https://en.wikipedia.org/wiki/Template:US_Army_navbox")
